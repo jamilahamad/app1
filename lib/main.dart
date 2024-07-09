e927f1d1e1d58f5165a55082809b006a41580622
dart <syljamil232@gmail.com>
2024-07-09 20:58:57 +0600

    cm

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(IntroApp());
+  runApp(MyApp());
 }
 
-class IntroApp extends StatelessWidget {
-  const IntroApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-      title: 'Intro app',
+      home: GreetingScreen(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
+class GreetingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
       appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blueAccent,
+        title: Text('Greeting App'),
       ),
       body: Center(
-        // child: Icon(
-        //   Icons.phone_android,
-        //   size: 40,
-        //   color: Colors.white,
-        // ),
-        //child: Image.network(https://www.freepik.com/free-ai-image/photorealistic-view-tree-nature-with-branches-trunk_186030404.htm#fromView=search&page=1&position=2&uuid=7ff8c03c-d4ea-482e-afc5-92c9fb7637a8),
-        // child: Image.asset(
-        //   'assets/images/audi.jpeg',
-        //   width: 250,
-        //   height: 200,
-        //   fit: BoxFit.cover,
-        // ),
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     print('Button pressed');
-        //   },
-        //   child: Text('Notification'),
-        // ),
-        // child: IconButton(
-        //   onPressed: () {
-        //     print('Add');
-        //   },
-        //   icon: Icon(Icons.add),
-        // ),
-        // child: TextButton(
-        //   onPressed: () {
-        //     print('Show button pressed');
-        //   },
-        //   child: Text('Text Button'),
-        // ),
-        /*child: GestureDetector(
-          onTap: () {
-            print('Singel tap');
-          },
-          onDoubleTap: () {
-            print('Double tap');
-          },
-          // child: Image.asset(
-          //   'assets/images/audi.jpeg',
-          //   width: 250,
-          //   height: 200,
-          //   fit: BoxFit.cover,
-          // ),
-          child: Text('Normal text'),
-        ),*/
-        child: InkWell(
-          onTap: () {
-            print('singel tap');
-          },
-          onDoubleTap: () {
-            print('double tap');
-          },
-          // child: Image.asset(
-          //   'assets/images/audi.jpeg',
-          //   width: 250,
-          //   height: 200,
-          //   fit: BoxFit.cover,
-          // ),
-          child: Text('normal tap'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Welcome to Flutter!',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            SizedBox(height: 16),
+            Image.asset(
+              'assets/images/flutter_logo.png', // Ensure you have the Flutter logo in your assets folder.
+              height: 100,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: Text('Press Me'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green
+                 // Button background color
+              ),
+            ),
+          ],
         ),
       ),
     );
