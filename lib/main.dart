e9e5932ad4da5858319cb2230947081439f2fc6f
dart <syljamil232@gmail.com>
2024-07-09 20:58:57 +0600

    cm

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class IntroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       home: Home(),
       title: 'Intro app',
     );
@@ -30,10 +31,9 @@ class Home extends StatelessWidget {
       body: Center(
         // child: Icon(
         //   Icons.phone_android,
-        //   size: 40,
+        //   size: 50,
         //   color: Colors.white,
         // ),
-        //child: Image.network(https://www.freepik.com/free-ai-image/photorealistic-view-tree-nature-with-branches-trunk_186030404.htm#fromView=search&page=1&position=2&uuid=7ff8c03c-d4ea-482e-afc5-92c9fb7637a8),
         // child: Image.asset(
         //   'assets/images/audi.jpeg',
         //   width: 250,
@@ -58,24 +58,23 @@ class Home extends StatelessWidget {
         //   },
         //   child: Text('Text Button'),
         // ),
-        /*child: GestureDetector(
-          onTap: () {
-            print('Singel tap');
-          },
-          onDoubleTap: () {
-            print('Double tap');
-          },
-          // child: Image.asset(
-          //   'assets/images/audi.jpeg',
-          //   width: 250,
-          //   height: 200,
-          //   fit: BoxFit.cover,
-          // ),
-          child: Text('Normal text'),
-        ),*/
+        // child: GestureDetector(
+        //   onTap: () {
+        //     print('Singel tap');
+        //   },
+        //   onDoubleTap: () {
+        //     print('Double tap');
+        //   },
+        //   child: Image.asset(
+        //     'assets/images/audi.jpeg',
+        //     width: 250,
+        //     height: 200,
+        //     fit: BoxFit.cover,
+        //   ),
+        // ),
         child: InkWell(
           onTap: () {
-            print('singel tap');
+            print('single tap');
           },
           onDoubleTap: () {
             print('double tap');
@@ -86,7 +85,7 @@ class Home extends StatelessWidget {
           //   height: 200,
           //   fit: BoxFit.cover,
           // ),
-          child: Text('normal tap'),
+          child: Text('Normal text'),
         ),
       ),
     );
